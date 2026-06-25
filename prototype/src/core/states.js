5d81f3bad63820aad609415a1de806dc90938b96
GUANJUNLIU <chris289742@gmail.com>
2026-06-25 12:43:53 +0800

    refactor state to class

--- prototype/src/core/states.js
+++ prototype/src/core/states.js
@@ -1,23 +1,34 @@
-export const state = {
-  allPieces: [],
-  selectedDateRange: null,
-  subscribers: [],
+class State {
+  constructor() {
+    this.allPieces = [];
+    this.selectedDateRange = null;
+    this.statusFilter = 'all';
+    this.subscribers = [];
+  }
 
   setPieces(pieces, silent = false) {
     this.allPieces = pieces;
     if (!silent) this.notify();
-  },
-  
+  }
+
   setDateRange(range) {
     this.selectedDateRange = range;
     this.notify();
-  },
+  }
+
+  setStatusFilter(filter) {
+    this.statusFilter = filter;
+    this.notify();
+  }
 
   subscribe(fn) {
     this.subscribers.push(fn);
-  },
+  }
 
   notify() {
     this.subscribers.forEach(fn => fn(this));
   }
-};
+}
+
+export const state = new State();
+
